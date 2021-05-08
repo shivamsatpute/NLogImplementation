8de83877802d7edd9e6b4ea870a26beb73addfe6
shivam satpute <shivam897satpute@gmail.com>
2021-05-08 08:35:03 +0530

    [shivam] add: NLog Implementation

---
+++ NLogImplimentation/AddNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogImplimentation
+{
+    public class AddNumbers
+    {
+        NLog nlog = new NLog();
+        public int Sum(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                nlog.LogDebug("Debug Successfull : sum()");
+                nlog.LogError("Expecting null value");
+                nlog.LogWarn("a or b should not equal to 0");
+            }
+            int c = a + b;
+            nlog.LogDebug("Debug Successful");
+            return c;
+
+
+        }
+    }
+    
+}
---
+++ NLogImplimentation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NLogImplimentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition problem \n Enter a, b values");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            AddNumbers add = new AddNumbers();
+            Console.WriteLine("result is" + " " + add.Sum(a,b));
+
+        }
+    }
+}
